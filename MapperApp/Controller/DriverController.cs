c7b59ffad97c5d8adfef4c3eba19410b3731134c
Antonio Rocha <antoniojunior159@gmail.com>
2022-10-17 18:39:21 -0300

    API Sem AutoMapper

---
+++ MapperApp/Controller/DriverController.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MapperApp.Models;
+
+namespace MapperApp.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DriverController : ControllerBase
+    {
+        private readonly ILogger<DriverController> _logger;
+
+        private static List<Driver> drivers = new List<Driver>();
+        public DriverController(ILogger<DriverController> logger)
+        {
+            _logger = logger;
+        }
+
+        //GET
+        [HttpGet]
+        [Route("GetDriver")]
+        public IActionResult GetDrivers()
+        {
+            var allDrivers = drivers.Where(x => x.Status == 1).ToList();
+            return Ok(allDrivers);
+        }
+
+        //POST
+        [HttpPost]
+        public IActionResult CreateDriver(Driver data)
+        {
+            if(ModelState.IsValid)
+            {
+                drivers.Add(data);
+                return CreatedAtAction("GetDrivers", new { data.Id }, data);
+            }
+
+            return new JsonResult("Something went Wrong") {StatusCode =500};
+        }
+
+        //GET Id
+        [HttpGet("{id}")]
+        public IActionResult GetDriverId(Guid id)
+        {
+            var item = drivers.FirstOrDefault(x => x.Id == id);
+
+            if (item == null)
+                return NotFound();
+
+            return Ok(item.Id);
+        }
+
+        //PUT
+        [HttpPut("{id}")]
+        public IActionResult UpdateDriver(Guid id, Driver data)
+        {
+            if (id == data.Id)
+                return BadRequest();
+
+            var existingDriver = drivers.FirstOrDefault(x => x.Id == id);
+
+            if (existingDriver == null)
+                return NotFound();
+
+            existingDriver.DriverNumber = data.DriverNumber;
+            existingDriver.FirstName = data.FirstName;
+            existingDriver.LastName = data.LastName;
+            existingDriver.WorldChampionships = data.WorldChampionships;
+
+            return NoContent();
+        }
+
+        //DELETE
+        [HttpDelete("{id}")]
+        public IActionResult DeleteDriver(Guid id)
+        {
+            var existingDriver = drivers.FirstOrDefault(x => x.Id == id);
+
+            if (existingDriver == null)
+                return NotFound();
+
+            existingDriver.Status = 0;
+
+            return NoContent();
+        }
+
+
+    }
+}
---
+++ MapperApp/Models/Driver.cs
@@ -0,0 +1,14 @@
+﻿namespace MapperApp.Models
+{
+    public class Driver
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int DriverNumber { get; set; }
+        public DateTime DateAdded { get; set; }
+        public DateTime DateUpdated { get; set; }
+        public int Status { get; set; }
+        public int WorldChampionships { get; set; }
+    }
+}
